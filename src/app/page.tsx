81db88add3ce4e2e36f8059ca07df0f68d976d85
Natanael Araújo <natanael@hotmail.co.jp>
2024-06-17 15:09:10 -0300

    refactor: components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,63 +1,25 @@
 'use client'
+import { CreateProductDialog } from '@/components/create-product-dialog';
+import { ProductsFilters } from '@/components/products-filters';
 import { Button } from '@/components/ui/button';
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
+import { Dialog, DialogTrigger } from '@/components/ui/dialog';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
-import { DialogClose } from '@radix-ui/react-dialog';
-import { Search, PlusCircle } from 'lucide-react';
+import { PlusCircle } from 'lucide-react';
 
 export default function Home() {
   return (
     <main className='p-6 max-w-4xl mx-auto space-y-4'>
       <h1 className='text-3xl font-bold'>Produtos</h1>
       <div className='flex flex-col md:flex-row md:items-center md:justify-between'>
-        <form className='flex flex-col md:flex-row md:items-center gap-2'>
-          <Input name='id' placeholder='ID do Pedido' className='w-auto'></Input>
-          <Input name='name' placeholder='Nome do produto' className='w-auto'></Input>
-          <Button type='submit' variant={'link'}>
-            <Search className='w-4 h-4 mr-2' />
-            Filtrar Resultados
-          </Button>
-        </form>
-
+        <ProductsFilters />
         <Dialog>
-
           <DialogTrigger asChild>
             <Button>
               <PlusCircle className='w-4 h-4 mr-2' />
               Novo Produto
             </Button>
           </DialogTrigger>
-
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle>Novo Produto</DialogTitle>
-              <DialogDescription>
-                Criar um novo produto no sistema
-              </DialogDescription>
-            </DialogHeader>
-
-            <form className='space-y-6'>
-              <div className='flex items-center md:text-right gap-3'>
-                <Label className='min-w-[80px]' htmlFor='name'>Produto</Label>
-                <Input className='col-span-3' id='name'></Input>
-              </div>
-
-              <div className='flex items-center md:text-right gap-3'>
-                <Label className='min-w-[80px]' htmlFor='price'>Preço</Label>
-                <Input className='w-full col-span-3' id='price'></Input>
-              </div>
-
-              <DialogFooter className='gap-3 md:gap-0'>
-                <DialogClose asChild>
-                  <Button type='button' variant={'outline'}>Cancelar</Button>
-                </DialogClose>
-                <Button type='submit'>Salvar</Button>
-              </DialogFooter>
-
-            </form>
-          </DialogContent>
+          <CreateProductDialog />
         </Dialog>
       </div>
       <div className='border rounded-lg p-2'>
